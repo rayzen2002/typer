a8710e7a8bcfad048e9265edcabfeda1a906ccb5
Emanuel Magalhães <emanuelzsmj@gmail.com>
2020-02-20 00:26:57 -0300

    Capítulo 4 do curso de jQuery

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,26 @@
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
-
-var tamanhoFrase = $("#tamanho-frase");
-tamanhoFrase.text(numPalavras);
-
+var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
-campo.on("input" , function(){
+
+$(
+	function(){
+	atualizaTamanhoFrase();
+	inicializaContadores();
+	inicializaCronometro();
+	$("#botao-reiniciar").click(reiniciaJogo);
+
+});
+
+function atualizaTamanhoFrase(){
+	var frase = $(".frase").text();
+	var numPalavras = frase.split(" ").length;
+
+	var tamanhoFrase = $("#tamanho-frase");
+	tamanhoFrase.text(numPalavras);
+}
+
+
+function inicializaContadores(){
+	campo.on("input" , function(){
 	var conteudo = campo.val();
 	var qtdPalavras = conteudo.split(/\S+/).length - 1;
 	console.log(qtdPalavras);
@@ -13,16 +28,31 @@ campo.on("input" , function(){
 	var qtdCaracteres = conteudo.length;
 	$("#contador-caracteres").text(qtdCaracteres);
 });
+}
 
-var tempoRestante = $("#tempo-digitacao").text();
-campo.one("focus" , function(){
-var cronometroID = setInterval(function(){
+function inicializaCronometro(){
+	var tempoRestante = $("#tempo-digitacao").text();
+	campo.one("focus" , function(){
+	var cronometroID = setInterval(function(){
 	tempoRestante --;
 	console.log(tempoRestante);
 	$("#tempo-digitacao").text(tempoRestante);
 	if(tempoRestante < 1 ){
 		campo.attr("disabled" , true);
 		clearInterval(cronometroID);
-	}
+		}
 	},1000);
-});
+	});
+}
+
+
+function reiniciaJogo(){
+	campo.attr("disabled" , false);
+	campo.val("");
+	$("#contador-caracteres").text("0");
+	$("#contador-palavras").text("0");
+	$("#tempo-digitacao").text(tempoInicial);
+	inicializaCronometro();
+};
+
+
