c130e94cced849d17c12e80c5a51e868c081382b
Emanuel Magalhães <emanuelzsmj@gmail.com>
2020-02-19 23:57:02 -0300

    Terceiro Capitulo curso jQuerry

--- public/js/main.js
+++ public/js/main.js
@@ -13,3 +13,16 @@ campo.on("input" , function(){
 	var qtdCaracteres = conteudo.length;
 	$("#contador-caracteres").text(qtdCaracteres);
 });
+
+var tempoRestante = $("#tempo-digitacao").text();
+campo.one("focus" , function(){
+var cronometroID = setInterval(function(){
+	tempoRestante --;
+	console.log(tempoRestante);
+	$("#tempo-digitacao").text(tempoRestante);
+	if(tempoRestante < 1 ){
+		campo.attr("disabled" , true);
+		clearInterval(cronometroID);
+	}
+	},1000);
+});
