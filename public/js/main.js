9f1d56afd35c3e9b5fb8b373a4e0157cafcfc0b8
Emanuel Magalhães <emanuelzsmj@gmail.com>
2020-02-20 01:27:45 -0300

    Capitulo 5 do curso de jQuery

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,13 @@
 var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
+var frase = $(".frase").text();
 
 $(
 	function(){
 	atualizaTamanhoFrase();
 	inicializaContadores();
 	inicializaCronometro();
+	inicializaMarcadores();
 	$("#botao-reiniciar").click(reiniciaJogo);
 
 });
@@ -23,7 +25,6 @@ function inicializaContadores(){
 	campo.on("input" , function(){
 	var conteudo = campo.val();
 	var qtdPalavras = conteudo.split(/\S+/).length - 1;
-	console.log(qtdPalavras);
 	$("#contador-palavras").text(qtdPalavras);
 	var qtdCaracteres = conteudo.length;
 	$("#contador-caracteres").text(qtdCaracteres);
@@ -35,11 +36,11 @@ function inicializaCronometro(){
 	campo.one("focus" , function(){
 	var cronometroID = setInterval(function(){
 	tempoRestante --;
-	console.log(tempoRestante);
 	$("#tempo-digitacao").text(tempoRestante);
 	if(tempoRestante < 1 ){
 		campo.attr("disabled" , true);
 		clearInterval(cronometroID);
+		campo.toggleClass("campo-desativado");
 		}
 	},1000);
 	});
@@ -53,6 +54,22 @@ function reiniciaJogo(){
 	$("#contador-palavras").text("0");
 	$("#tempo-digitacao").text(tempoInicial);
 	inicializaCronometro();
+	campo.toggleClass("campo-desativado");
+	campo.removeClass("campo-correto");
+	campo.removeClass("campo-errado");
 };
 
+function inicializaMarcadores(){
+	campo.on("input" , function(){
+		var digitado = campo.val();
+		var comparavel = frase.substr(0,digitado.length);
+		if (digitado == comparavel){
+			campo.addClass("campo-correto");
+			campo.removeClass("campo-errado");
+		}else{
+			campo.addClass("campo-errado");
+			campo.removeClass("campo-certo");
+		}
+	});
+};
 
